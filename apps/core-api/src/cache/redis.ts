376a6f8b15e15e7d9aba2fea4bc43c080694d6d0
ShiljaBabu <shiljababu78@gmail.com>
2026-01-28 15:55:49 +0530

    feat: add Redis connection setup in core-api

---
+++ apps/core-api/src/cache/redis.ts
@@ -0,0 +1,16 @@
+import IORedis from "ioredis";
+import type {Redis} from "ioredis";
+
+import { logger } from "../utils/logger.js";
+const redis_url = process.env.REDIS_URL
+if (!redis_url){
+    throw new Error("REDIS_URL is not defined in environment variables")
+}
+export const redis:Redis = new IORedis.default(redis_url)
+redis.on("connect", ()=>{
+    logger.info("Redis connected successfully")
+})
+
+redis.on("error", (err:unknown)=>{
+    logger.error(`Redis error:${String(err)}`)
+})
