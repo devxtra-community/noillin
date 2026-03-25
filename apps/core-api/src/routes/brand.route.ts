f0bd0d7822e2586c39d1b1d92662c8cc6edc1c02
Safwan <safuwanfazz@gmail.com>
2026-03-25 20:07:57 +0530

    feat(signup):meilisearch

--- apps/core-api/src/routes/brand.route.ts
+++ apps/core-api/src/routes/brand.route.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 import { getPublicBrandProfileController } from "../controllers/brand.controller.js";
 
-const router = Router();
+const router: Router = Router();
 
 router.get("/:brandId", getPublicBrandProfileController);
 
