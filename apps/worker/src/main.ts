eb7dcdacaba117e760006bc4699c1be20b2553a8
Shiljababu <shiljababu78@gmail.com>
2026-04-10 09:40:14 +0530

    refractor:a small fix in the gig worker to make it stable and production ready

--- apps/worker/src/main.ts
+++ apps/worker/src/main.ts
@@ -3,11 +3,17 @@ import 'dotenv/config';
 import { connectRabbit } from "./rabbit/connection";
 import { startGigConsumer } from "./consumers/gig.consumer";
 import { logger } from "./utils/logger";
+import { startBrandConsumer } from './consumers/brand.cosumer';
+import { startInfluencerConsumer } from './consumers/influencer.consumer';
 async function bootstrap() {
   try {
     const channel = await connectRabbit();
 
-    await startGigConsumer(channel);
+    await Promise.all([
+      startGigConsumer(channel),
+      startBrandConsumer(channel),
+      startInfluencerConsumer(channel),
+    ])
 
     logger.info("Worker started successfully");
   } catch (err) {
